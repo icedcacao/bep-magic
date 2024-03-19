879c23cc16c772fc93217307e0392876e193506e
icedcacao <icedcacaogaming@gmail.com>
2024-03-19 08:48:28 -0700

    adding tailwind sorter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,15 @@ export default defineNuxtConfig({
     mongoUri: process.env.MONGO_URI,
     imageXApiKey: process.env.IMAGE_X_API_KEY,
   },
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config",
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: "first",
+    viewer: true,
+  },
   auth: {
     isEnabled: true,
     baseURL: process.env.AUTH_ORIGIN,
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>nice</h1>
+  <h1 class="container mx-auto text-red-300">nice</h1>
 </template>
 
 <script setup lang="ts"></script>
