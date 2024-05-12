09dec79ec7cc6e0c59e8f0421ec0bb17c751ed1c
icedcacao <icedcacaogaming@gmail.com>
2024-05-12 01:24:25 -0700

    f

--- src/repositories/userRepo.js
+++ src/repositories/userRepo.js
@@ -1,19 +1,19 @@
-const User = require("../models/User");
+// const User = require("../models/User");
 
-const findOne = async (condition) => {
-  try {
-    return await User.findOne(condition).lean();
-  } catch (error) {
-    throw error;
-  }
-};
+// const findOne = async (condition) => {
+//   try {
+//     return await User.findOne(condition).lean();
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-const findById = async (id) => {
-  try {
-    return await User.findById(id);
-  } catch (error) {
-    throw error;
-  }
-};
+// const findById = async (id) => {
+//   try {
+//     return await User.findById(id);
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-module.exports = { findOne, findById };
+// module.exports = { findOne, findById };
